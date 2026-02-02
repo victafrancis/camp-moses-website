22760762eaeba22ba95f66a3a4087862909175b1
Francis Victa <francisvicta45@gmail.com>
2026-02-02 10:01:53 -0500

    bugfix: image not loading on large desktop screen

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   },
   images: {
     formats: ['image/webp', 'image/avif'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 2720],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 }
